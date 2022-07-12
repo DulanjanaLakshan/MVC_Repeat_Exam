7f082ee1f1629745a56e7d1c78ae231eb22af1cd
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-07-12 10:38:54 +0530

    view ok

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class StudentFormController {
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtNIC;
+    public Button btnSaveOnAction;
+    public Button btnUpdateOnAction;
+    public Button btnDeleteOnAction;
+    public TableView tblStudent;
+    public TableColumn colID;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colEmail;
+    public TableColumn colNic;
+    public TableColumn colContact;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
 }
