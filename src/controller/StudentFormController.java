e02c6a71133f4bb4e1fe4c72f64c935bd89a6b88
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-07-12 11:18:40 +0530

    load All Student for Tabale

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,13 +1,24 @@
 package controller;
 
+import db.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Student;
+import view.tm.StudentTm;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class StudentFormController implements Initializable {
@@ -20,7 +31,7 @@ public class StudentFormController implements Initializable {
     public Button btnSaveOnAction;
     public Button btnUpdateOnAction;
     public Button btnDeleteOnAction;
-    public TableView tblStudent;
+    public TableView<StudentTm> tblStudent;
     public TableColumn colID;
     public TableColumn colName;
     public TableColumn colAddress;
@@ -30,11 +41,53 @@ public class StudentFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        tblStudent.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblStudent.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblStudent.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("email"));
+        tblStudent.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("contact"));
+        tblStudent.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("address"));
+        tblStudent.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("nic"));
+        loadAllStudent();
 
+        tblStudent.getSelectionModel().selectedItemProperty().addListener((observable,oldValue,newValue)->{
+            txtID.setText(newValue.getId());
+            txtName.setText(newValue.getName());
+            txtEmail.setText(newValue.getEmail());
+            txtContact.setText(newValue.getContact());
+            txtAddress.setText(newValue.getAddress());
+            txtNIC.setText(newValue.getNic());
+        });
     }
 
-    private void loadAllCustomer(){
+    private void loadAllStudent() {
+        ArrayList<Student> students = new ArrayList<>();
+        ObservableList<StudentTm> objects = FXCollections.observableArrayList();
 
+        try {
+            Connection connection = DBConnection.getDbConnection().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Student");
+            ResultSet rst = preparedStatement.executeQuery();
+
+            while (rst.next()) {
+                students.add(new Student(
+                        rst.getString(1),
+                        rst.getString(2),
+                        rst.getString(3),
+                        rst.getString(4),
+                        rst.getString(5),
+                        rst.getString(6))
+                );
+            }
+            for (Student stu : students) {
+                objects.add(new StudentTm(stu.getId(), stu.getName(), stu.getEmail(), stu.getContact(), stu.getAddress(), stu.getNic()));
+            }
+            tblStudent.setItems(objects);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
