23b3da2298ccc802a4e1edcdcd754ad2999eb6b8
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-07-12 10:09:10 +0530

    techar 5 insert quary ok

---
+++ insertquary.sql
@@ -0,0 +1,15 @@
+use ijse;
+INSERT INTO Teacher (teacher_id, name, nic, contact, address)
+VALUES ('T001','Nipuna','123456789','0175454561','kalutara');
+
+INSERT INTO Teacher (teacher_id, name, nic, contact, address)
+VALUES ('T002','Nipuna','123456789','0175454561','kalutara');
+
+INSERT INTO Teacher (teacher_id, name, nic, contact, address)
+VALUES ('T003','Nipuna','123456789','0175454561','kalutara');
+
+INSERT INTO Teacher (teacher_id, name, nic, contact, address)
+VALUES ('T004','Nipuna','123456789','0175454561','kalutara');
+
+INSERT INTO Teacher (teacher_id, name, nic, contact, address)
+VALUES ('T005','Nipuna','123456789','0175454561','kalutara');
